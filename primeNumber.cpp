624313b622b48c3a263af74e83d91ae75e7bd304
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-01 15:08:43 +0530

    program to find whether a  number is Prime Number or Not

---
+++ primeNumber.cpp
@@ -0,0 +1,34 @@
+// find a number is prime or not given by the user
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int num, i;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num < 2)                        // numbers which are less than 2 can not be a prime number
+    {
+        cout << "Not a Prime Number!";
+        return 0;
+    }
+    else
+    {
+        for(i=2; i<num; i++)
+        {
+            if(num % i == 0)                    // if the number is divisible from any number between 1 and itself then it can not be a Prime number
+            {
+                cout << "Not a Prime Number!";
+                return 0;
+            }
+        }
+
+        cout << "Prime Number!";
+    }
+
+    return 0;
+}
