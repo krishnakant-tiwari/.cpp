d582705b077574173e2637522ab9c8be389d5096
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-02 15:32:15 +0530

    printing solid square characters using loop

--- patternPrinting/solidSquareNumber6.cpp
+++ patternPrinting/solidSquareNumber6.cpp
@@ -4,23 +4,49 @@
 // a b c d e
 // a b c d e
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
 
-    int row;
-    char col;
+    int row, col;
+    char name;
 
-    for (row = 1; row <= 5; row = row + 1)
+    for(row=1; row<=5; row=row+1)
     {
-        for (col = 'a'; col <= 'e'; col = col + 1)
+        for(col=1; col<=5; col=col+1)
         {
-            cout << col << " ";
+            name = 'a' + (col - 1);
+            {
+                cout << name << " ";
+            }
         }
         cout << endl;
     }
 
     return 0;
 }
+
+                            //   Another way 
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int row;
+//     char col;
+
+//     for (row = 1; row <= 5; row = row + 1)
+//     {
+//         for (col = 'a'; col <= 'e'; col = col + 1)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
