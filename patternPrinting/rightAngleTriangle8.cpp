4146bdafdf98cd92c53339728c65b2de9de4dc06
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-03 16:07:47 +0530

    printing solid right angle triangle pattern

--- patternPrinting/rightAngleTriangle8.cpp
+++ patternPrinting/rightAngleTriangle8.cpp
@@ -14,12 +14,35 @@ int main()
 
     for(row=1; row<=5; row=row+1)
     {
-        for(col=5; col>=row; col=col-1)
+        for(col=5; col>=5-(row-1); col=col-1)
         {
             cout << col << " ";
         }
         cout << endl;
+        
     }
 
     return 0;
 }
+
+                                     // Another way
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int row, col;
+
+//     for(row=1; row<=5; row=row+1)
+//     {
+//         for(col=5; col>=row; col=col-1)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
