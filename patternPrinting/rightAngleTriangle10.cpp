6bf1ab91ad877c4a661dd5bd1fd3f979790fffc0
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-04 19:55:15 +0530

    adding comments

--- patternPrinting/rightAngleTriangle10.cpp
+++ patternPrinting/rightAngleTriangle10.cpp
@@ -17,10 +17,13 @@ cin >> n;
 
 for(row=1; row<=n; row=row+1)
 {
+    //print space
     for(col=1; col<=n-row; col=col+1)
     {
         cout << "  ";
     }
+
+    //print number
     for(col=1; col<=row; col=col+1)
     {
         cout << row << " ";
