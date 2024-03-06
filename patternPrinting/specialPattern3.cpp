e818145a34cbaf8cb1e170a0e304a7140b8c7b17
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-06 21:51:28 +0530

    printing a special pattern using loop

---
+++ patternPrinting/specialPattern3.cpp
@@ -0,0 +1,55 @@
+//    *
+//   * *
+//  * * *
+// * * * *
+// * * * *
+//  * * * 
+//   * *
+//    *
+   
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int row, col;
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    for(row=1; row<=n; row=row+1)
+    {   
+        //print space
+        for(col=1; col<=n-row; col=col+1)
+        {
+            cout << " ";
+        }
+
+        //print *
+        for(col=1; col<=row; col=col+1)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    for(row=n; row>=1; row=row-1)
+    {   
+        //print space
+        for(col=1; col<=n-row; col=col+1)
+        {
+            cout << " ";
+        }
+
+        //print *
+        for(col=1; col<=row; col=col+1)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
