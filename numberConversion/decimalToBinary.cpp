aac20015a516f700ce64df78001c6c759d865877
Krishna Kant Tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-10 22:13:41 +0530

    adding comments

--- numberConversion/decimalToBinary.cpp
+++ numberConversion/decimalToBinary.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main()
 {
-    int num;
-    int rem, ans = 0, mul = 1;
+    int num;                  //declaring number variable to take decimal input from user
+    int rem, ans = 0, mul = 1;// declaring remainder, answer and multiply variable initializing ans with 0 and mul with 1
 
-    cout << "Enter a decimal number: ";
+    cout << "Enter a decimal number: "; //Taking input from the user
     cin >> num;
 
-    while(num > 0)
+    while(num > 0)    // loop break condition
     {
-        rem = num % 2;
-        num = num / 2;
-        ans = rem * mul + ans;
-        mul = mul * 10;
+        rem = num % 2;         // extracting remainder
+        num = num / 2;         // extracting new number 
+        ans = rem * mul + ans; // extract current answer
+        mul = mul * 10;        // incrementing mul * 10 for every iteration
     }
 
-    cout << "Binary form = " << ans;
+    cout << "Binary form = " << ans;      //printing binary form of the given decimal number
 
 
 
