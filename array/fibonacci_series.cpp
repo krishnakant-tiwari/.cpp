3646423993a45ebb12c1a023763939eb4232ba81
krishnakant-tiwari <tkrishnakanttiwari6@gmail.com>
2024-03-20 20:35:19 +0530

    program to get any element of fibonacci series

---
+++ array/fibonacci_series.cpp
@@ -0,0 +1,27 @@
+// program to find nth fibbonacci number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int arr[1000];
+
+    //initialize first element with 0
+    arr[0] = 0;
+    // initialize second element with 1
+    arr[1] = 1;
+
+    for(int i=2; i<=n-1; i++)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    cout << arr[n-1];
+
+    return 0;
+}
